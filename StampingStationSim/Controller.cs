742ebbb454a479aaf5e3bab50af9a7d70be220fc
TadIsGoat <tadeasiknet@gmail.com>
2026-03-12 09:50:06 +0100

    manual mode added

--- StampingStationSim/Controller.cs
+++ StampingStationSim/Controller.cs
@@ -30,110 +30,122 @@ namespace StampingStationSim
         private int maxMovementTime = 2000;
         public void Update(Inputs inputs, Outputs outputs)
         {
-            switch (currentState)
+            if (inputs.manualModeSwitch)
             {
-                case State.Idle:
-                    if (inputs.startButton && inputs.partPresentSensor)
-                    {
-                        outputs.activeLight = true;
-                        outputs.extendClamp = true;
-                        currentState = State.ClampExtending;
-                        movementTimer.Restart();
-                    }
-                    break;
-                case State.ClampExtending:
-                    if (movementTimer.ElapsedMilliseconds >= maxMovementTime)
-                    {
-                        currentState = State.Fault;
-                    }
-                    else if (inputs.clampExtendedSensor)
-                    {
-                        outputs.extendClamp = false;
-                        currentState = State.ClampExtended;
-                        movementTimer.Stop();
-                        clampTimer.Restart();
-                    }
-                    break;
-                case State.ClampExtended:
-                    if (clampTimer.ElapsedMilliseconds >= clampTime)
-                    {
-                        outputs.extendStamp = true;
-                        currentState = State.StampExtending;
-                        movementTimer.Restart();
-                    }
-                    break;
-                case State.StampExtending:
-                    if (movementTimer.ElapsedMilliseconds >= maxMovementTime)
-                    {
-                        currentState = State.Fault;
-                    }
-                    else if (inputs.stampExtendedSensor)
-                    {
-                        outputs.extendStamp = false;
-                        currentState = State.StampExtended;
-                        movementTimer.Stop();
-                        dwellTimer.Restart();
-                    }
-                    break;
-                case State.StampExtended:
-                    if (dwellTimer.ElapsedMilliseconds >= dwellTime)
-                    {
-                        outputs.retractStamp = true;
-                        currentState = State.StampRetracting;
-                        movementTimer.Restart();
-                    }
-                    break;
-                case State.StampRetracting:
-                    if (movementTimer.ElapsedMilliseconds >= maxMovementTime)
-                    {
-                        currentState = State.Fault; 
-                    }
-                    else if (inputs.stampRetractedSensor)
-                    {
-                        outputs.retractStamp = false;
-                        currentState = State.StampRetracted;
-                        movementTimer.Stop();
-                        clampTimer.Restart();
-                    }
-                    break;
-                case State.StampRetracted:
-                    if (clampTimer.ElapsedMilliseconds >= clampTime)
-                    {
-                        outputs.retractClamp = true;
-                        currentState = State.ClampRetracting;
-                        movementTimer.Restart();
-                    }
-                    break;
-                case State.ClampRetracting:
-                    if (movementTimer.ElapsedMilliseconds >= maxMovementTime)
-                    {
-                        currentState = State.Fault;
-                    }
-                    else if (inputs.clampRetractedSensor)
-                    {
-                        outputs.retractClamp = false;
-                        currentState = State.ClampRetracted;
-                        movementTimer.Stop();
-                    }
-                    break;
-                case State.ClampRetracted:
-                    outputs.activeLight = false;
-                    outputs.ResetAll();
-                    currentState = State.Idle;
-                    break;
-                case State.Fault:
-                    outputs.extendStamp = false;
-                    outputs.retractStamp = false;
-                    outputs.extendClamp = false;
-                    outputs.retractClamp = false;
+                outputs.extendClamp = inputs.clampExtendButton && !inputs.clampRetractButton;
+                outputs.retractClamp = inputs.clampRetractButton && !inputs.clampExtendButton;
+                outputs.extendStamp = inputs.stampExtendButton && !inputs.stampRetractButton;
+                outputs.retractStamp = inputs.stampRetractButton && !inputs.stampExtendButton;
 
-                    if (inputs.resetButton)
-                    {
-                        outputs.retractStamp = true;
-                        currentState = State.StampRetracting;
-                        movementTimer.Restart();
-                    }
-                    break;
+                currentState = State.Fault; // to force the operator to press reset after exiting manual mode
+            }
+            else
+            {
+                switch (currentState)
+                {
+                    case State.Idle:
+                        if (inputs.startButton && inputs.partPresentSensor)
+                        {
+                            outputs.activeLight = true;
+                            outputs.extendClamp = true;
+                            currentState = State.ClampExtending;
+                            movementTimer.Restart();
+                        }
+                        break;
+                    case State.ClampExtending:
+                        if (movementTimer.ElapsedMilliseconds >= maxMovementTime)
+                        {
+                            currentState = State.Fault;
+                        }
+                        else if (inputs.clampExtendedSensor)
+                        {
+                            outputs.extendClamp = false;
+                            currentState = State.ClampExtended;
+                            movementTimer.Stop();
+                            clampTimer.Restart();
+                        }
+                        break;
+                    case State.ClampExtended:
+                        if (clampTimer.ElapsedMilliseconds >= clampTime)
+                        {
+                            outputs.extendStamp = true;
+                            currentState = State.StampExtending;
+                            movementTimer.Restart();
+                        }
+                        break;
+                    case State.StampExtending:
+                        if (movementTimer.ElapsedMilliseconds >= maxMovementTime)
+                        {
+                            currentState = State.Fault;
+                        }
+                        else if (inputs.stampExtendedSensor)
+                        {
+                            outputs.extendStamp = false;
+                            currentState = State.StampExtended;
+                            movementTimer.Stop();
+                            dwellTimer.Restart();
+                        }
+                        break;
+                    case State.StampExtended:
+                        if (dwellTimer.ElapsedMilliseconds >= dwellTime)
+                        {
+                            outputs.retractStamp = true;
+                            currentState = State.StampRetracting;
+                            movementTimer.Restart();
+                        }
+                        break;
+                    case State.StampRetracting:
+                        if (movementTimer.ElapsedMilliseconds >= maxMovementTime)
+                        {
+                            currentState = State.Fault;
+                        }
+                        else if (inputs.stampRetractedSensor)
+                        {
+                            outputs.retractStamp = false;
+                            currentState = State.StampRetracted;
+                            movementTimer.Stop();
+                            clampTimer.Restart();
+                        }
+                        break;
+                    case State.StampRetracted:
+                        if (clampTimer.ElapsedMilliseconds >= clampTime)
+                        {
+                            outputs.retractClamp = true;
+                            currentState = State.ClampRetracting;
+                            movementTimer.Restart();
+                        }
+                        break;
+                    case State.ClampRetracting:
+                        if (movementTimer.ElapsedMilliseconds >= maxMovementTime)
+                        {
+                            currentState = State.Fault;
+                        }
+                        else if (inputs.clampRetractedSensor)
+                        {
+                            outputs.retractClamp = false;
+                            currentState = State.ClampRetracted;
+                            movementTimer.Stop();
+                        }
+                        break;
+                    case State.ClampRetracted:
+                        outputs.activeLight = false;
+                        outputs.ResetAll();
+                        currentState = State.Idle;
+                        break;
+                    case State.Fault:
+                        outputs.extendStamp = false;
+                        outputs.retractStamp = false;
+                        outputs.extendClamp = false;
+                        outputs.retractClamp = false;
+
+                        if (inputs.resetButton)
+                        {
+                            outputs.retractStamp = true;
+                            currentState = State.StampRetracting;
+                            movementTimer.Restart();
+                        }
+                        break;
+                }
             }
         }
     }
--- StampingStationSim/Inputs.cs
+++ StampingStationSim/Inputs.cs
@@ -8,11 +8,20 @@ namespace StampingStationSim
     {
         public bool startButton { get; private set; }
         public bool resetButton { get; private set; }
+
         public bool partPresentSensor { get; private set; }
+
         public bool stampExtendedSensor {  get; private set; }
         public bool stampRetractedSensor { get; private set; }
         public bool clampExtendedSensor { get; private set; }
         public bool clampRetractedSensor { get; private set; }
+
+        public bool manualModeSwitch { get; private set; }
+
+        public bool clampExtendButton { get; private set; }
+        public bool clampRetractButton { get; private set; }
+        public bool stampExtendButton { get; private set; }
+        public bool stampRetractButton { get; private set; }
 
         public void ReadInputs(bool stampExtended, bool stampRetracted, bool clampExtended, bool clampRetracted)
         {
@@ -36,6 +45,30 @@ namespace StampingStationSim
                 {
                     partPresentSensor = !partPresentSensor;
                 }
+                else if (key == ConsoleKey.M)
+                {
+                    manualModeSwitch = !manualModeSwitch;
+                }
+
+                if (manualModeSwitch)
+                {
+                    if (key == ConsoleKey.NumPad1)
+                    {
+                        clampExtendButton = true;
+                    }
+                    if (key == ConsoleKey.NumPad2)
+                    {
+                        clampRetractButton = true;
+                    }
+                    if (key == ConsoleKey.NumPad3)
+                    {
+                        stampExtendButton = true;
+                    }
+                    if (key == ConsoleKey.NumPad4)
+                    {
+                        stampRetractButton = true;
+                    }
+                }
             }
         }
 
@@ -43,10 +76,20 @@ namespace StampingStationSim
         {
             startButton = false;
             resetButton = false;
+
+            //partPresentSensor not here cuy its a switch
+
             stampExtendedSensor = false;
             stampRetractedSensor = false;
             clampExtendedSensor = false;
             clampRetractedSensor = false;
+
+            //manualModeButton is a switch too
+
+            clampExtendButton = false;
+            clampRetractButton = false;
+            stampExtendButton = false;
+            stampRetractButton = false;
         }
     }
 }
