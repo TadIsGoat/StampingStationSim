555c715849bcbe61079ed677be8538ea5860c649
TadIsGoat <tadeasiknet@gmail.com>
2026-03-12 10:08:48 +0100

    .

--- StampingStationSim/obj/Debug/net10.0/StampingStationSim.AssemblyInfo.cs
+++ StampingStationSim/obj/Debug/net10.0/StampingStationSim.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StampingStationSim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be0d6db74967b75bb3663e575060f199c4400b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+742ebbb454a479aaf5e3bab50af9a7d70be220fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("StampingStationSim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StampingStationSim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
